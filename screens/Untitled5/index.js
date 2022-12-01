0b32399e53029d6f0c351e86dfab6d9f3d9b366c
teynali@teynali.com <teynali@teynali.com>
2022-12-01 10:37:05 -0500

    Crowdbotics Studio: Update app screens

--- screens/Untitled5/index.js
+++ screens/Untitled5/index.js
@@ -1,18 +1,86 @@
+import { TextInput } from "react-native";
+import { Text } from "react-native";
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
 const Untitled5 = () => {
   return <View style={_styles.YtzVXevu}>
-      </View>;
+      <Text style={_styles.CrzQFvlg}>Email</Text><Text style={_styles.TYmMJduQ}>Password</Text><TextInput style={_styles.hXPQrlHV}></TextInput><TextInput style={_styles.MEuJKrmD}></TextInput></View>;
 };
 
 export default Untitled5;
 
 const _styles = StyleSheet.create({
   YtzVXevu: {
-    backgroundColor: "#f0f0f1",
+    backgroundColor: "#FFFFFF",
     padding: 10,
     position: "relative",
     height: "100%"
+  },
+  NgeQuNaJ: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0,
+    position: "absolute",
+    left: 120,
+    top: 189
+  },
+  CrzQFvlg: {
+    left: 31,
+    top: 249,
+    position: "absolute",
+    width: 60,
+    height: 24,
+    lineHeight: 14,
+    fontSize: 16,
+    borderRadius: 0,
+    letterSpacing: 2,
+    fontWeight: "500"
+  },
+  ecxVwcJK: {
+    left: 19,
+    top: 250,
+    position: "absolute",
+    width: 60,
+    height: 24,
+    lineHeight: 14,
+    fontSize: 16,
+    borderRadius: 0,
+    letterSpacing: 2,
+    fontWeight: "500"
+  },
+  TYmMJduQ: {
+    left: 23,
+    top: 309,
+    position: "absolute",
+    width: 90,
+    height: 26,
+    lineHeight: 16,
+    fontSize: 16,
+    borderRadius: 0,
+    letterSpacing: 2,
+    fontWeight: "500"
+  },
+  hXPQrlHV: {
+    left: 125,
+    top: 245,
+    position: "absolute",
+    backgroundColor: "#eae5e5",
+    borderColor: "#cccccc",
+    width: 196,
+    height: 31,
+    color: "#ADD8E6"
+  },
+  MEuJKrmD: {
+    left: 125,
+    top: 298,
+    position: "absolute",
+    backgroundColor: "#eae5e5",
+    borderColor: "#cccccc",
+    width: 196,
+    height: 31,
+    color: "#ADD8E6"
   }
 });
--- screens/Untitled5Copy/index.js
+++ screens/Untitled5Copy/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
-const Untitled6 = () => {
-  return <View style={_styles.gULHSMxc}>
+const Untitled5Copy = () => {
+  return <View style={_styles.YtzVXevu}>
       </View>;
 };
 
-export default Untitled6;
+export default Untitled5Copy;
 
 const _styles = StyleSheet.create({
-  gULHSMxc: {
-    backgroundColor: "#FFFFFF",
+  YtzVXevu: {
+    backgroundColor: "#f0f0f1",
     padding: 10,
     position: "relative",
     height: "100%"
