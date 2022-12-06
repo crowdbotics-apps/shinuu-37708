20951c1bacfebbd74e1187d8c1d3657586b1593b
teynali@teynali.com <teynali@teynali.com>
2022-12-06 16:19:59 -0500

    Crowdbotics Studio: Update app screens

--- screens/GmailLogInCopy/index.js
+++ screens/GmailLogInCopy/index.js
@@ -156,20 +156,20 @@ const _styles = StyleSheet.create({
     position: "absolute",
     left: 103.5,
     top: 375,
-    borderWidth: 2
+    borderWidth: 3
   },
   RofUiLoR: {
     width: 69,
     height: 22,
     lineHeight: 16,
-    fontSize: 14,
+    fontSize: 16,
     borderRadius: 0,
     color: "#000080",
     letterSpacing: 2,
-    fontWeight: "900",
+    fontWeight: "500",
     position: "absolute",
-    left: 40.5,
-    top: 12,
+    left: 33,
+    top: 10,
     textAlign: "center"
   }
 });
